f9cfb0424c0a4e80292f5e36b8ba0640f8a930ea
SergioV <svargas7910@gmail.com>
2025-02-09 14:48:23 -0500

    Se agrego el calculo de promedio, max, min y su visualizacion

--- Reto_1.py
+++ Reto_1.py
@@ -1,4 +1,9 @@
 import re
+#VARIABLES----------------------------------------------------------------------    
+List_Materiales = []
+List_MatSort = []
+Minimo = {}
+Maximo = {}
 
 #FUNCIONES----------------------------------------------------------------------
 def Val_fecha(fecha):
@@ -42,67 +47,66 @@ def Val_resultado (resultado):
 
 def add_exp(fecha, material, resultado1, resultado2, resultado3, resultado4 ): # Creal el diccionario del material
 
-    Dic_exp = {material : [resultado1, resultado2, resultado3, resultado4, fecha]} 
+    Dic_exp = {"Fecha": fecha, 
+               "Material": material, 
+               "Resultados" : [resultado1, resultado2, resultado3, resultado4]} 
     return Dic_exp
 
 def add_To_listMat(Dict_Material: dict):
     
     List_Materiales.append(Dict_Material)
 
-    return List_Materiales
-
 def Show_listMat(List_Materiales: list):
 
-    print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format("#","Material", "250 Hz", "500Hz", "1KHz", "2KHz", "Fecha"))
-    print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format("---","---------", "------", "-----", "-----", "-----", "----------"))
+    print("{:<5} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10}".format("#","Fecha","Material", "250 Hz", "500 Hz", "1 KHz", "2 KHz"))
+    print("{:<5} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10}".format("---","----------","---------","------","------","------","------"))
     x = 0
 
-    for pos in List_Materiales:
+    for dict in List_Materiales:
         x += 1
-        for key,value in pos.items():
-            print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(x, key, value[0], value[1], value[2], value[3], value[4]))
+        print("{:<5} {:<12} {:<10} {:<10} {:<10} {:<10} {:<10}".format(x, dict["Fecha"], dict["Material"], dict["Resultados"][0], dict["Resultados"][1], dict["Resultados"][2], dict["Resultados"][3]))
 
 def Edit_name(List_Materiales: list, indice_mat, New_name):
 
     indice_mat -= 1 # Actualiza el indice para buscar la posicion correcta en la lista
-    Names=list(List_Materiales[indice_mat].keys()) # Obtiene lista de nombres
-
-    List_Materiales[indice_mat][New_name] = List_Materiales[indice_mat].pop(Names[0]) #Reemplaza el nombre
-
-    return List_Materiales
+    List_Materiales[indice_mat]["Material"] = New_name
 
 def Edit_result(List_Materiales: list, indice_mat, new_r1, new_r2, new_r3, new_r4):
 
     indice_mat -= 1 # Actualiza el indice para buscar la posicion correcta en la lista
-
-    for key,value in List_Materiales[indice_mat].items(): #Itera y accede al diccionario 
-        value[0] = new_r1 # Se reemplaza el valor actual por el valor nuevo
-        value[1] = new_r2
-        value[2] = new_r3
-        value[3] = new_r4
     
-    return List_Materiales
-
+    List_Materiales[indice_mat]["Resultados"][0]= new_r1
+    List_Materiales[indice_mat]["Resultados"][1]= new_r2
+    List_Materiales[indice_mat]["Resultados"][2]= new_r3
+    List_Materiales[indice_mat]["Resultados"][3]= new_r4
+    
 def Edit_date(List_Materiales: list, indice_mat, New_date):
     
     indice_mat -= 1 # Actualiza el indice para buscar la posicion correcta en la lista
-
-    for key,value in List_Materiales[indice_mat].items(): #Itera y accede al diccionario 
-        value[4] = New_date # Se reemplaza el valor actual por el valor nuevo
     
-    return List_Materiales
-
-
+    List_Materiales[indice_mat]["Fecha"] = New_date
+ 
 def Del_listMat(List_Materiales: list, indice_mat):
 
     indice_mat -= 1 # Actualiza el valor del indice para acceder a la posicion real de la lista
     List_Materiales.pop(indice_mat) # Elimina el material en la posicion escogida
     print("\nMaterial eliminado correctamente")  
 
-    return List_Materiales #Actualiza la lista de materiales
+def Calc_NRC(List_Materiales: list):
+    
+    for dict in List_Materiales:
+        Promedio1 = lambda a,b,c,d : (a+b+c+d)/4 #Funcion que realiza el promedio de los datos
+        dict["NRC"]= Promedio1(dict["Resultados"][0],dict["Resultados"][1],dict["Resultados"][2],dict["Resultados"][3]) # Se agrega el promedio a la lista de materiales
 
-#VARIABLES----------------------------------------------------------------------    
-List_Materiales = []
+def Show_listRst(List_Materiales: list):
+
+    print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format("#","Material", "250 Hz", "500 Hz", "1 KHz", "2 KHz", "NRC")) # Imprime titulos 
+    print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format("---","---------","------","------","------","------","------")) # Imprime separadores 
+    x = 0
+
+    for dict in List_Materiales:
+        x += 1
+        print("{:<5} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(x, dict["Material"], dict["Resultados"][0], dict["Resultados"][1], dict["Resultados"][2], dict["Resultados"][3], dict["NRC"])) # Imprime los resultados
 
 #PROGRAMA ----------------------------------------------------------------------
 while True:
@@ -112,9 +116,10 @@ while True:
     print("2. Ver lista de materiales")
     print("3. Modificar material")
     print("4. Eliminar material")
-    print("5. Cálcular NRC y RT Mid")
+    print("5. Cálcular NRC del material")
     print("6. Comparacion de materiales")
-    print("7. Salir")
+    print("7. Exportar informacion")
+    print("8. Salir")
     
     menu = int(input("\nSeleccione una opción del menu: "))
 
@@ -139,8 +144,11 @@ while True:
         #Val_resultado(resultado)
 
         Dict_Material = add_exp(fecha, material, resultado1, resultado2, resultado3, resultado4) #Creamos un diccionario del material agregado
+
         add_To_listMat(Dict_Material)
         print("\nMaterial agregado correctamente")
+
+        Calc_NRC(List_Materiales) #Calcula el promedio de resultados de una vez
 
 
     elif menu == 2: # VER MATERIALES AGREGADOS
@@ -153,52 +161,111 @@ while True:
             print("\nAun no se han agregado materiales al programa - Intente de nuevo")
         
     elif menu == 3: # EDITAR MATERIAL
-        Show_listMat(List_Materiales) # Muestra los experimentos agregados
-        indice_mat = int(input("\n¿Que material desea modificar?:"))
-        
-        while True: # Menu de acciones
 
-            print("\n¿Que item desea modificar ?: ")
-            print("1. Nombre del material")
-            print("2. Resultados")
-            print("3. Fecha")
-            print("4. Regresar al menú principal")
-            menu = int(input("\nSeleccione una opción: "))
+        if len(List_Materiales) != 0:
 
-            if menu == 1:
-
-                new_name=input("Ingrese el nuevo nombre del material: ")
-                Edit_name(List_Materiales, indice_mat, new_name)
-
-            elif menu == 2:
-
-                new_r1 = float(input("\nIngrese la abs en la banda de 250 Hz: "))
-                new_r2 = float(input("\nIngrese la abs en la banda de 500 Hz: "))
-                new_r3 = float(input("\nIngrese la abs en la banda de 1KHz: "))
-                new_r4 = float(input("\nIngrese la abs en la banda de 2KHz: "))
-                Edit_result(List_Materiales, indice_mat, new_r1, new_r2, new_r3, new_r4)
+            Show_listMat(List_Materiales) # Muestra los experimentos agregados
+            indice_mat = int(input("\n¿Que material desea modificar?:"))
             
-            elif menu == 3:
-                
-                new_date = input("Ingrese la nueva fecha del experimento: ")
-                Edit_date(List_Materiales, indice_mat, new_date)
+            while True: # Menu de acciones
 
-            elif menu == 4:
-                break
+                print("\n¿Que item desea modificar ?: ")
+                print("1. Nombre del material")
+                print("2. Resultados")
+                print("3. Fecha")
+                print("4. Regresar al menú principal")
+                menu = int(input("\nSeleccione una opción: "))
+
+                if menu == 1:
+
+                    while True:
+
+                        new_name=input("Ingrese el nuevo nombre del material: ")# Solicita el nuevo nombre
+
+                        if Val_nombre(new_name): #Valida que el nombre sea correcto
+                            break
+
+                    Edit_name(List_Materiales, indice_mat, new_name) #Edita únicamente el nombre del material según el indice dado
+
+                elif menu == 2:
+
+                    new_r1 = float(input("\nIngrese la abs en la banda de 250 Hz: "))
+                    new_r2 = float(input("\nIngrese la abs en la banda de 500 Hz: "))
+                    new_r3 = float(input("\nIngrese la abs en la banda de 1KHz: "))
+                    new_r4 = float(input("\nIngrese la abs en la banda de 2KHz: "))
+                    Edit_result(List_Materiales, indice_mat, new_r1, new_r2, new_r3, new_r4)
+                
+                elif menu == 3:
+                    
+                    while True:
+
+                        new_date = input("Ingrese la nueva fecha del experimento: ") # Solicita la nueva fecha
+                        
+                        if Val_fecha(new_date): #Valida que la fecha sea correcta 
+                            break
+
+                    Edit_date(List_Materiales, indice_mat, new_date) #Edita únicamente la fecha del experimento según el indice dado
+
+                elif menu == 4:
+                    break
+
+        else:
+
+            print("\nAun no se han agregado materiales al programa - Intente de nuevo")
 
 
 
     elif menu == 4: # ELIMINAR MATERIAL
 
-        Show_listMat(List_Materiales) # Muestra nuevamente la lista de materiales
+        if len(List_Materiales) != 0:
 
-        indice_mat = int(input("\nCon base en la lista, ingrese el indice del material que desea eliminar: ")) # Solicita el material que se quiere eliminar
-        Del_listMat(List_Materiales, indice_mat)       
+            Show_listMat(List_Materiales) # Muestra nuevamente la lista de materiales
 
-    else:
+            indice_mat = int(input("\nCon base en la lista, ingrese el indice del material que desea eliminar: ")) # Solicita el material que se quiere eliminar
+            Del_listMat(List_Materiales, indice_mat)
 
-        print(menu)
+        else:
 
-    if menu == 7:
+            print("\nAun no se han agregado materiales al programa - Intente de nuevo")          
+
+    elif menu == 5: # CALCULO NRC
+
+        if len(List_Materiales) != 0:
+
+            Show_listRst(List_Materiales) # Muestra la lista de materiales con el promedio de resultados
+
+        else:
+
+            print("\nAun no se han agregado materiales al programa - Intente de nuevo")
+
+    elif menu == 6:
+
+        if len(List_Materiales) != 0:
+
+            Minimo = min(List_Materiales, key=lambda k: k["NRC"]) # Evalua cual valor de NRC de todos los diccionarios es el menor
+            Maximo = max(List_Materiales, key=lambda k: k["NRC"]) # Evalua cual valor de NRC de todos los diccionarios es el mayor
+            List_MatSort = sorted(List_Materiales, key=lambda k: k["NRC"]) # Busca el valor de la llave en cada diccionario de la lista y ordena de mneor a mayor
+            
+            print("\nA continuación se presentan los materiales ordenados de menor a mayor absorción acústica: ")
+            Show_listRst(List_MatSort)
+
+            print("\nEl mayor valor de NRC es de: ", Maximo["NRC"], "y corresponde al material: ", Maximo["Material"])
+            print("El menor valor de NRC es de: ", Minimo["NRC"], "y corresponde al material: ", Minimo["Material"])
+
+        else:
+             
+             print("\nAun no se han agregado materiales al programa - Intente de nuevo")
+
+
+    elif menu == 7:
+        
+        if len(List_Materiales) != 0 and len(List_MatSort) != 0:
+            file = open("Informe de Resultados - Absorción Acústica","w")
+            file. write("")
+            file.close()
+        else:
+            print("\nAun no se han agregado materiales al programa - Intente de nuevo")
+
+    if menu == 8:
         print("\nFin del programa")
         break
